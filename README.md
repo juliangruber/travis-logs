17f1a570ef08c5ffe0b7dd760e9c48d59c45f068
Julian Gruber <julian@juliangruber.com>
2017-05-02 11:25:57 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# travis-logs
+
+Stream travis logs of the current repository's last commit's last build to the terminal.
+
+## Usage
+
+```bash
+$ cd ~/dev/level/leveldown
+$ travis-logs
+
+$ # or
+
+$ travis-logs ~/dev/level/leveldown
+```
+
+## Installation
+
+```bash
+$ npm install -g travis-logs
+```
+
+## License
+
+MIT
+
+
---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+'use strict'
+
+const getCommit = require('git-current-commit').promise
+const getRepo = require('gh-canonical-repository').promise
+const getBuild = require('travis-build-by-commit')
+const { https } = require('follow-redirects')
+
+const dir = process.argv[2] || process.cwd()
+
+Promise.all([
+  getRepo(dir),
+  getCommit(dir)
+])
+.then(([ repo, sha ]) => getBuild({ repo, sha }))
+.then(build => {
+  const job = build.job_ids[build.job_ids.length - 1]
+  https.get(`https://api.travis-ci.org/jobs/${job}/log`, res => {
+    if (res.statusCode !== 200) throw Error(`Status: ${res.statusCode}`)
+    res.pipe(process.stdout)
+  })
+})
