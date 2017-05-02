b0257fffef226c4847a3d142c93fcc5996308ea1
Julian Gruber <julian@juliangruber.com>
2017-05-02 12:18:52 +0200

    so progress

--- index.js
+++ index.js
@@ -5,14 +5,41 @@ const getCommit = require('git-current-commit').promise
 const getRepo = require('gh-canonical-repository').promise
 const getBuild = require('travis-build-by-commit')
 const { https } = require('follow-redirects')
+const Travis = require('travis-ci')
 
+const travis = new Travis({ version: '2.0.0' })
 const dir = process.argv[2] || process.cwd()
+
+const failure = () => {
+  console.error('Build failed!')
+  process.exit(1)
+}
+
+const passed = () => {
+  console.log('Build passed!')
+  process.exit(0)
+}
 
 Promise.all([getRepo(dir), getCommit(dir)])
   .then(([repo, sha]) => getBuild({ repo, sha }))
   .then(build => {
-    const job = build.job_ids[build.job_ids.length - 1]
-    https.get(`https://api.travis-ci.org/jobs/${job}/log`, res => {
+    if (build.state === 'failed') failure()
+    else if (build.state === 'passed') passed()
+    return build
+  })
+  .then(build => Promise.all(build.job_ids.map(id => new Promise((resolve, reject) => {
+     const cb = (err, res) => err ? reject(err) : resolve(res.job)
+     travis.jobs(id).get(cb)
+  }))))
+  .then(jobs => {
+    const job = jobs.find(job => job.state !== 'failed' && job.state !== 'passed')
+    https.get({
+      host: 'api.travis-ci.org',
+      path: `/jobs/${job.id}/log`,
+      headers: {
+        'Accept': 'text/plain chunked=true'
+      }
+    }, res => {
       if (res.statusCode !== 200) throw Error(`Status: ${res.statusCode}`)
       res.pipe(process.stdout)
     })
